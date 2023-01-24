f5e26d5372cba25086607f3cdf39346ca1156fce
Roderis Ortega <78380781+Roderis27x@users.noreply.github.com>
2023-01-24 12:11:59 -0500

    Dia 2

--- Codigo/backend.py
+++ Codigo/backend.py
@@ -1,14 +1,21 @@
 #Se importaran las librerias para crear la IA y el modelo de la red neuronal
-
-import numpy as np
+import json
 import nltk
 import tensorflow
 import tflearn
 import random
+import numpy as np
 
 #Se importan las librerias para el preprocesamiento de los datos
 from nltk.stem.lancaster import LancasterStemmer
+from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
+
+
+with open('intents.json') as file:
+    data = json.load(file)
+
+data
 
 #Se crea una lista para las palabras
 words = []
@@ -16,83 +23,79 @@ labels = []
 docs_x = []
 docs_y = []
 
-#Se importan los datos de los archivos json
-import json
-with open("banco.json") as file:
-    data = json.load(file)
 
+for intents in data['intents']:
+    for patterns in intents['patterns']:
 
-#Se recorre el archivo json para obtener las palabras y las etiquetas
-for banco in data["banco"]:
-    for pattern in banco["patterns"]:
-        wrds = nltk.word_tokenize(pattern)
+        wrds = nltk.word_tokenize(patterns)
         words.extend(wrds)
         docs_x.append(wrds)
-        docs_y.append(banco["tag"])
+        docs_y.append(intents["tag"])
 
-    if banco["tag"] not in labels:
-        labels.append(banco["tag"])
+        if intents['tag'] not in labels:
+            labels.append(intents['tag'])
 
-#Se limpian las palabras y se ordenan
 
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+
+
+words = sorted(list(set(words)))
 words = [stemmer.stem(w.lower()) for w in words if w != "?"]
 words = sorted(list(set(words)))
 labels = sorted(labels)
 
-#Se crea el modelo de la red neuronal y se entrena
 training = []
 output = []
+
 out_empty = [0 for _ in range(len(labels))]
 
 for x, doc in enumerate(docs_x):
-    bag=[]
-    wrds = [stemmer.stem(w) for w in doc]
+    bag = []
+
+    wrds = [stemmer.stem(w.lower()) for w in doc]
 
     for w in words:
         if w in wrds:
+
             bag.append(1)
         else:
+
             bag.append(0)
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
-    training.append(bag)
-    output.append(output_row)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
 
 #convertir lista a arreglos
 training = np.array(training)
 output = np.array(output)
 
+tensorflow.compat.v1.reset_default_graph()
+
 #Crear modelo de red neuronal
 net = tflearn.input_data(shape=[None, len(training[0])])
-net = tflearn.fully_connected(net, 10)
-net = tflearn.fully_connected(net, 10)
-net = tflearn.fully_connected(net, 10)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
 net = tflearn.regression(net)
 
-#Entrenar modelo+
 model = tflearn.DNN(net)
+#model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+#model.save("model.tflearn")
 
-try:
-    model.load("model.tflearn")
-except:                         
-    model.fit(training, output, n_epoch = 5000, batch_size = 10, show_metric = True)
-    model.save("model.tflearn")
+def bag_of_words(s, words):
 
-def bankWords(s, words):
     bag = [0 for _ in range(len(words))]
-    
+
     s_words = nltk.word_tokenize(s)
     s_words = [stemmer.stem(word.lower()) for word in s_words]
-    
+
     for se in s_words:
         for i, w in enumerate(words):
             if w == se:
                 bag[i] = 1
-                
+
     return np.array(bag)
-    
-
-
-
-
---
+++ Codigo/telegram.py
@@ -0,0 +1,33 @@
+
+import telebot
+import random
+import numpy as np
+from backend import bag_of_words, model, words, labels, data
+
+bot = telebot.TeleBot('5937779519:AAHQpSMYurGkcjw4-M_LPeJS6lDdgfsTBIE')
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+   # Procesar el mensaje y determinar la intención del usuario
+    message_text = message.text  # obtenemos el texto del mensaje
+    # creamos la representacion numerica del mensaje
+    message_representation = bag_of_words(message_text, words)
+    # hacemos la prediccion con el modelo entrenado
+    result = model.predict([message_representation])
+    # obtenemos el indice de la intencion con mayor probabilidad
+    intent_index = np.argmax(result)
+    if intent_index != None:
+        intent = labels[intent_index]  # obtenemos la intencion correspondiente
+        # Seleccionar una respuesta apropiada
+        for intents in data['intents']:
+            if intents['tag'] == intent:
+                responses = intents['responses']
+        # Seleccionar una respuesta al azar
+        response = random.choice(responses)
+    else:
+        response = "I'm sorry, I didn't understand your message. Could you please clarify?"
+    # Enviar respuesta al usuario
+    bot.send_message(chat_id=message.chat.id, text=response)
+
+
+bot.polling()
